cfedcfc63690450fb583303c76ae590b26073e3b
sedsaid <sed@wearewhy.co.uk>
2018-12-21 17:37:09 +0000

    Learning the Python 3 Library - chap0101 complete.

--- Learning_the_Python_3_Standard_Library/Exercise Files/Ch01/01_01/01_01_Start.py
+++ Learning_the_Python_3_Standard_Library/Exercise Files/Ch01/01_01/01_01_Start.py
@@ -1,8 +1,12 @@
 # Python Logical Operators: And, Or, Not:
 
 # What is a Boolean?
+# A binary variable that evalutes to True or False
+isRaining = True
+isSunny = False
 
 # Logical Operators -> Special Operators for Booleans
+# Takes one or more Boolean values and operates on them.
 
 # AND
 # true and true --> true
@@ -10,12 +14,27 @@
 # true and false --> false
 # false and false --> false
 
+if isRaining and isSunny: 
+    print('We might see a rainbow')
+
 # OR
 # true and true --> true
 # false and true --> true
 # true and false --> true
 # false and false --> false
 
+if isRaining or isSunny:
+    print('It might be rainy or it might be sunny')
+
 # NOT
 # true --> false
 # false --> true
+
+if not isRaining:
+    print("It must be raining")
+
+ages = [12, 18, 39, 87, 7, 2]
+for age in ages:
+    isAdult = age > 17
+    if not isAdult:
+        print("Being " + str(age) + " does not make you an adult.")
