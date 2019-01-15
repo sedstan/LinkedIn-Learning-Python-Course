f89bc86ef43c016ffe1ad12e1726c631c7e13e61
sedsaid <sed@wearewhy.co.uk>
2019-01-15 13:13:17 +0000

    Added exercise files and updated settings files.

---
+++ Programming_Foundations_Real_World_Examples/Exercise Files/Ch01/01_01/start_01_01_breakfast_functions.py
@@ -0,0 +1,16 @@
+""" A Functional Breakfast """
+
+def make_omlette():
+    print('Mixing the ingredients')
+    print('Pouring the mixture into a frying pan')
+    print('Cooking the first side')
+    print('Flipping it!')
+    print('Cooking the other side')
+    omelette = 'a tasty omelette'
+    return omelette 
+
+
+omelette1 = make_omlette()
+print(omelette1)
+omelette2 = make_omlette()
+print(omelette2)
---
+++ Programming_Foundations_Real_World_Examples/Exercise Files/Ch01/functions.md
@@ -0,0 +1,9 @@
+# Functions
+
+A routine or procedure that may or may not taken in processes and may or may not return a value.
+
+Why? or Should or should not? Used for commonly used routines.
+
+## Scope
+
+Looks for local scope first and global second. Creates the local variable at runtime as to not override the global variable.
